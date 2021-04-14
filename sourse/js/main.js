1a2d65f15f68bccc0bb7cbed2eac9e5cd14dc4db
Sokolovsky <reco.357@yandex.ru>
2021-04-14 17:50:16 +0300

    Почти закончил писать функционал

--- sourse/js/main.js
+++ sourse/js/main.js
@@ -1,32 +1,49 @@
-var swiper = new Swiper(".swiper-container-menu", {
-    slidesPerView: 2,
-    initialSlide: 1,
-    spaceBetween: 30,
-    centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    }
+    var galleryThumbs = new Swiper(".gallery-thumbs", {
+      spaceBetween: 20,
+      slidesPerView: 4,
+      loop: true,
+      freeMode: false,
+      loopedSlides: 4, //looped slides should be the same
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true
+    });
+
+    var galleryTop = new Swiper(".gallery-top", {
+        spaceBetween: 10,
+        loop: true,
+        loopedSlides: 4, //looped slides should be the same
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        thumbs: {
+            swiper: galleryThumbs
+        }
+    });
+    
+    var swiper = new Swiper(".swiper-container-menu", {
+        slidesPerView: 2,
+        initialSlide: 1,
+        spaceBetween: 30,
+        centeredSlides: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        }
+      });
+
+  $('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+   
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
   });
 
-  var galleryThumbs = new Swiper(".gallery-thumbs", {
-    spaceBetween: 20,
-    slidesPerView: 4,
-    loop: true,
-    freeMode: false,
-    loopedSlides: 4, //looped slides should be the same
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true
-  });
-  var galleryTop = new Swiper(".gallery-top", {
-    spaceBetween: 10,
-    loop: true,
-    loopedSlides: 4, //looped slides should be the same
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    thumbs: {
-      swiper: galleryThumbs
-    }
-  });
+ $('.video-play').click(function(){
+    $('video')[0].play();
+    });
+
+// $('.player').click(function(){
+//     $('video')[0].pause();
+//     });
